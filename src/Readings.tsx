b7fe417449b23c4b3cd6e16f470437313fe9da37
Patrick King <patrick.f.king@gmail.com>
2021-10-12 02:07:16 -0600

    Add a readings chart component, aggregate the readings data as needed for two charts (temperature and humidity). Functional but not fully baked.

--- src/Readings.tsx
+++ src/Readings.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-
 import { useState, useEffect } from 'react';
-import { ReadingData } from './RemoteDataTypes';
-import { requestReadingData } from './Requests'
 
+
+import ReadingsChart from './ReadingsChart'
+import { SensorData, ReadingData } from './RemoteDataTypes';
+import { requestSensorData, requestReadingData } from './Requests'
 
 
 
 function Readings () {
+
+  const [sensorData, setSensorData] = useState<null | [SensorData]>(null);
+
+  useEffect( () => {
+    requestSensorData(setSensorData)
+  }, [setSensorData])
 
   const [readingData, setReadingData] = useState<null | [ReadingData]>(null);
 
@@ -15,8 +22,25 @@ function Readings () {
     requestReadingData(setReadingData)
   }, [setReadingData])
 
-  return <div></div>
+  if (sensorData === null || readingData == null) {
+    return <div>
+      <span> Loading ... </span>
+    </div>
+  }
 
+
+  return <div>
+    <ReadingsChart 
+      type = { 'Temperature Sensor' }
+      sensorData = { sensorData }
+      readingData = { readingData }
+    />
+    <ReadingsChart 
+      type = { 'Humidity Sensor' }
+      sensorData = { sensorData }
+      readingData = { readingData }
+    />
+  </div>
 }
 
 export default Readings
---
+++ src/ReadingsChart.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { VictoryChart, VictoryLine, VictoryTheme } from 'victory';
+import Moment from 'moment'
+import Numeral from 'numeral'
+
+import { SensorData, ReadingData } from './RemoteDataTypes';
+
+
+// Create a map of sensor id => sensor, with only sensors of the given type.
+function sensorMapOfType (sensors: SensorData[], sensorType: string) {
+
+  const sensorsMap = new Map<number, SensorData>();
+
+  for (const sensor of sensors) {
+    if (sensor.type === sensorType) {
+      sensorsMap.set(sensor.id, sensor)
+    }
+  }
+
+  return sensorsMap;
+}
+
+
+// Given a map of sensor id -> sensor, create a map of sensor id -> readings, for only the specified sensors
+function datasetsForSensors (readingData: ReadingData[], sensorsMap: Map<number, SensorData>) {
+
+  const datasets = new Map<number, ReadingData[]>();
+
+  for (const item of readingData) {
+
+    const sensor = sensorsMap.get(item.sensorId)
+
+    if (!sensor) {
+      continue;
+    }
+
+    if (!datasets.get(item.sensorId)) {
+      datasets.set(item.sensorId, []) 
+    }
+
+    (datasets.get(item.sensorId) as ReadingData[]).push(item)
+
+  }
+
+  return datasets;
+}
+
+// Transform reading data into the shape required by Victory
+function processReadingData (readingData: ReadingData[]) {
+
+  return readingData.map ( reading => {
+    return {
+      x: Moment(reading.time).unix(),
+      y: reading.value//.toFixed(1)
+    }
+  })
+
+}
+
+type ReadingsChartProps = {
+  type: string,
+  sensorData: SensorData[],
+  readingData: ReadingData[],
+}
+
+function ReadingsChart (props: ReadingsChartProps) {
+
+  const sensorsMap = sensorMapOfType(props.sensorData, props.type)
+  const datasets = datasetsForSensors(props.readingData, sensorsMap)
+
+
+  return <VictoryChart theme = { VictoryTheme.material }>
+    { Array.from(datasets).map( ([key, dataset]) => {
+        return <VictoryLine data = { processReadingData(dataset) }>
+        </VictoryLine>
+      })
+    }
+
+  </VictoryChart>
+
+
+}
+
+
+
+export default ReadingsChart
